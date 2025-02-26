f37d20b44f304127ea1a8003efa5261d140a1001
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-02-26 16:48:22 +0200

    created a class to create, display and remove cookies

--- public/app.js
+++ public/app.js
@@ -39,6 +39,60 @@ try {
 	const decodedValue = getCookieValue('my_encoded_cookie');
 	console.log('Decoded value:', decodedValue);
 } catch (error) {
-  // Handle the error appropriately (e.g., use the encoded value or a default)
+	// Handle the error appropriately (e.g., use the encoded value or a default)
 	console.error(error);
 }
+
+// Custom Cookie Class
+class Cookie {
+	static get(name) {
+		const cookieName = `${encodeURIComponent(name)}=`;
+		const cookie = document.cookie;
+		let value = null;
+
+		const startIndex = cookie.indexOf(cookieName);
+
+		if (startIndex > -1) {
+			let endIndex = cookie.indexOf(';', startIndex);
+
+			if (endIndex == -1) {
+				endIndex = cookie.length;
+			}
+
+			value = decodeURIComponent(
+				cookie.substring(startIndex + name.length + 1, endIndex)
+			);
+		}
+
+		return value;
+	}
+
+	static set(name, value, expires, path, domain, secure) {
+		let cookieText = `${encodeURIComponent(name)}=${encodeURIComponent(value)}`;
+		if (expires instanceof Date) {
+			cookieText += `; expires=${expires.toGMTString()}`;
+		}
+
+		if (path) cookieText += `; path=${path}`;
+		if (domain) cookieText += `; domain=${domain}`;
+		if (secure) cookieText += `; secure`;
+
+		document.cookie = cookieText;
+	}
+
+	static remove(name, path, domain, secure) {
+		Cookie.set(name, '', new Date(0), path, domain, secure);
+	}
+}
+
+// set expiry data
+const date = new Date();
+
+// set a cookie
+Cookie.set('username', 'admin', date.setTime(date.getTime() + 7 * 24 * 60 * 60 * 1000));
+
+// get a cookie
+console.log(Cookie.get('username')); // admin
+
+// remove a cookie by a name
+Cookie.remove('username');
