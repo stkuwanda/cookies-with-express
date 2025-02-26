fcc953217cbd803febfb1261bc2f942d9d515e56
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-02-26 10:51:35 +0200

    implemented and used a custom middleware to set a cookie

--- server.js
+++ server.js
@@ -9,12 +9,23 @@ const serveStaticFilesMiddleware = express.static(staticFilesPath);
 const bodyParserMiddleware = express.urlencoded({ extended: true });
 const jsonParserMiddleware = express.json();
 const cookieParserMiddleware = cookieParser();
+const setCookieMiddleware = (req, res, next) => {
+  res.cookie('hello', 'world', { maxAge: 60000 * 60 });
+  next();
+}
 
 const app = express();
 
-app.use(loggerMiddleware, serveStaticFilesMiddleware, bodyParserMiddleware, jsonParserMiddleware, cookieParserMiddleware);
+app.use(
+	loggerMiddleware,
+  setCookieMiddleware,
+	serveStaticFilesMiddleware,
+	bodyParserMiddleware,
+	jsonParserMiddleware,
+	cookieParserMiddleware
+);
 
-
+// error handling
 app.use((err, req, res, next) => {
 	console.error(err.stack);
 	res.status(500).send('Internal server error');
