f690d71d9c3a2d58e4254da6aa752393447161ab
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-02-26 10:09:03 +0200

    added logging and cookie-parsing middleware

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 import path from 'node:path';
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
 const staticFilesPath = path.join(process.cwd(), 'public');
+const loggerMiddleware = logger('dev');
 const serveStaticFilesMiddleware = express.static(staticFilesPath);
 const bodyParserMiddleware = express.urlencoded({ extended: true });
 const jsonParserMiddleware = express.json();
+const cookieParserMiddleware = cookieParser();
 
 const app = express();
 
-app.use(bodyParserMiddleware, jsonParserMiddleware, serveStaticFilesMiddleware);
+app.use(loggerMiddleware, serveStaticFilesMiddleware, bodyParserMiddleware, jsonParserMiddleware, cookieParserMiddleware);
 
 
 app.use((err, req, res, next) => {
